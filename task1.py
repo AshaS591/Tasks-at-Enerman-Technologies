c7504013eaed8e4d1be7a2a546b9d6b1cf60323b
Asha S <as1579119@gmail.com>
2025-04-15 10:28:42 +0530

    updated code

--- task1.py
+++ task1.py
@@ -6,10 +6,18 @@
 # - Name
 # - Roll Number
 # - Marks for 3 subjects: Math, Physics, Chemistry
-
-name = input('Enter your name : ')
-roll_no = input('Enter your roll_no : ')
-marks = eval(input('Enter marks for 3 subjects: Math, Physics, Chemistry as dictionary :'))
+student_data1={}
+student_data2={}
+student_data3={}
+student_data4={}
+students=[student_data1,student_data2,student_data3,student_data4]
+for student in students:
+    name = input('Enter your name : ')
+    roll_no = input('Enter your roll_no : ')
+    marks = eval(input('Enter marks for 3 subjects: Math, Physics, Chemistry as dictionary :'))
+    student['name']=name
+    student['roll_no']=roll_no
+    student['marks']=marks
 
 
 
@@ -19,14 +27,16 @@ marks = eval(input('Enter marks for 3 subjects: Math, Physics, Chemistry as dict
 # "roll_no": "101",
 # "marks": {"Math": 90, "Physics": 85, "Chemistry": 80}
 # }
-student_data={}
-student_data['name']=name
-student_data['roll_no']=roll_no
-student_data['marks']=marks
+
+
 
 # 3. Append each student dictionary to a list.
 student_list=[]
-student_list.append(student_data)
+for student in students:
+    student_list.append(student)
+
+print(student_list)
+
 
 # 4. Print all student data using a loop.
 # Expected Output: Name: John
@@ -41,4 +51,5 @@ student_list.append(student_data)
 for data in student_list:
     
     print(f'Name: {data['name']}\nRoll No: {data['roll_no']}\nMarks: \nMaths: {data['marks']['Math']}\nPhysics: {data['marks']['Physics']}\nChemistry: {data['marks']['Chemistry']}')
+    print()
 
